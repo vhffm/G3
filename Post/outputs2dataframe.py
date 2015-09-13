d9905a508855edeb6353e1e054fa1b97d93a0072
Volker Hoffmann <volker@cheleb.net>
2015-09-13 10:22:52 +0200

    perf: parallelize out2df conversion

--- Post/outputs2dataframe.py
+++ Post/outputs2dataframe.py
@@ -6,13 +6,54 @@ Dirlist Format:
 /path/02/
 ...
 /path/NN/
+
+Code is embarrassingly parallel. Use the -np to define number of subprocesses.
 """
 
 import io_helpers as ioh
 import pandas as pd
 import sys
 import glob
+import multiprocessing as mp
+import argparse
 
+
+###############################################################################
+# FUNCTION DEFINITIONS
+###############################################################################
+
+def outs2df(cdir):
+    """
+    Processing Function.
+    """
+
+    print "** %s" % cdir
+
+    # Glob Coordinate Output Files
+    globs = glob.glob("%s/Out_*.dat" % cdir)
+    globs = sorted(globs)
+
+    # Load Data
+    df = ioh.read_output_and_stack(globs, frame="heliocentric", \
+                                   drop_duplicates=False)
+    df.reset_index(drop=True, inplace=True)
+
+    # Return Dataframe
+    return df
+
+
+###############################################################################
+# MAIN PROGRAM STARTS HERE
+###############################################################################
+
+# Parse Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-np', type=int, default=1, \
+                    help='Number of Processes')
+parser.add_argument('-fout', '--output_file', default='Resonances.hdf5', \
+                    help='Name of Output File.')
+args = parser.parse_args()
+print "// Using %i Subprocesses" % args.np
 
 # List of Directories
 if sys.stdin.isatty():
@@ -27,19 +68,21 @@ else:
 
 
 # Loop Directories
-for idir, cdir in enumerate(dirs):
-    print "** %02d/%02d" % (idir+1, len(dirs))
+if args.np == 1:
+    result = []
+    for idir, cdir in enumerate(dirs):
+        df_tmp = outs2df(cdir)
+        result.append(df_tmp)
+else:
+    pool = mp.Pool(processes=args.np)
+    result = pool.map(outs2df, dirs)
+    pool.close()
+    pool.join()
 
-    # Glob Coordinate Output Files
-    globs = glob.glob("%s/Out_*.dat" % cdir)
-    globs = sorted(globs)
-
-    df = ioh.read_output_and_stack(globs, frame="heliocentric", \
-                                   drop_duplicates=False)
-    df.reset_index(drop=True, inplace=True)
-
-    # Save
-    with pd.HDFStore("Coordinates_%02d.hdf5" % int(idir+1)) as store:
+# Save
+print "// Saving Dataframes"
+for idf, df in enumerate(result):
+    with pd.HDFStore("Coordinates_%02d.hdf5" % int(idf+1)) as store:
         store["df"] = df
 
 # Done
