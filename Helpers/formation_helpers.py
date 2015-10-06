217f2cc967e0efb2fe4cfca1d144ed56caaafecb
Volker Hoffmann <volker@cheleb.net>
2015-10-06 13:34:52 +0200

    feat: initial water mass fractions

--- Helpers/formation_helpers.py
+++ Helpers/formation_helpers.py
@@ -25,3 +25,47 @@ def return_sources(pid, dfc):
             sources.append(int(dfc_loc.pidi))
     sources = np.asarray(sources, dtype=np.int64)
     return sources
+
+
+def assign_wmf_raymond2004(a):
+    """
+    Cf. Sec. 2.2 and Fig. 2 in Raymond+ (2004).
+    http://adsabs.harvard.edu/abs/2004Icar..168....1R
+
+    @param: a - Semi Major Axis (Float)
+    @return: wmf - Water Mass Fraction (Float)
+    """
+    # percent
+    if a < 2.0:
+        wmf = 0.001
+    elif a >= 2.0 and a < 2.5:
+        wmf = 0.1
+    elif a>= 2.5:
+        wmf = 5.0
+    # fraction
+    wmf /= 100.0
+    # return
+    return wmf
+
+
+def assign_wmf_ronco2014(a):
+    """
+    Cf. Sec. 2 in Ronco+ (2014).
+    http://adsabs.harvard.edu/abs/2015arXiv150907217R
+
+    @param: a - Semi Major Axis (Float)
+    @return: wmf - Water Mass Fraction (Float)
+    """
+    # percent
+    if a < 2.0:
+        wmf = 0.001
+    elif a >= 2.0 and a < 2.5:
+        wmf = 0.1
+    elif a>= 2.5 and a < 2.7:
+        wmf = 5.0
+    else:
+        wmf = 50.0
+    # fraction
+    wmf /= 100.0
+    # return
+    return wmf
