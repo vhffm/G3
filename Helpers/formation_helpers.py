ef8d0de0c3e3861edfd51fcabbf647bc5c6338e0
Volker Hoffmann <volker@cheleb.net>
2015-10-11 09:00:29 +0200

    fix: mrange => evaluation_range

--- Helpers/formation_helpers.py
+++ Helpers/formation_helpers.py
@@ -128,7 +128,7 @@ def compute_kde(df, evaluation_range, evaluation_range_step, variable, \
 
     # Normalize to Total Number of Particles (Int{dN/dM} = N)
     else:
-        dmx = np.diff(10.0**mrange) # Midpoints in mrange, convert to lin
+        dmx = np.diff(10.0**evaluation_range) # Midpoints in mrange, convert to lin
         kde_evaluated_x = \
             (kde_evaluated[1:] + kde_evaluated[:-1]) / 2.0 # Linear Interp.
         N_0 = np.sum(kde_evaluated_x * dmx)
